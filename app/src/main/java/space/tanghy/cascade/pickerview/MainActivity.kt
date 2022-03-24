75343eace12b62b0946c886b50ecaa108d46e06b
飘香随云 <wozaifeiyang0@163.com>
2022-03-24 11:18:00 +0800

    更新说明

--- app/src/main/java/space/tanghy/cascade/pickerview/MainActivity.kt
+++ app/src/main/java/space/tanghy/cascade/pickerview/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.widget.AppCompatTextView
-import space.tanghy.cascade.lib.dialog.CascadePickerView
 import space.tanghy.cascade.lib.dialog.CityPickerView
-import space.tanghy.cascade.lib.dialog.Item
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
